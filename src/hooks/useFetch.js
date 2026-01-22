6300fe0b65ef25e071278aaf1c48f63a926178fe
linh <linhphuongpham1711@gmail.com>
2026-01-22 10:11:37 +0700

    xu li URL trong

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,4 +4,16 @@ export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+    useEffect(() => {
+    if (!url) {
+      setData(null);
+      setLoading(false);
+      setError(null);
+      return;
+    }
+    return () => {
+      isCurrent = false;
+    };
+  }, [url]);
 }
