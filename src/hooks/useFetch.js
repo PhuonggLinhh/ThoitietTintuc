045a2ad5e267682da7ec0b094696354ea85391b0
linh <linhphuongpham1711@gmail.com>
2026-01-22 10:09:24 +0700

    khoi tao trang thai useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+}
