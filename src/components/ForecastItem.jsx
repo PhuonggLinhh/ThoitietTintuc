0234b32a7b80c6a25971011ff3d34e6e64dd2696
linh <linhphuongpham1711@gmail.com>
2026-01-22 10:54:57 +0700

    them hien thi du bao thoi tiet hang ngay

--- src/components/ForecastItem.jsx
+++ src/components/ForecastItem.jsx
@@ -4,4 +4,19 @@ export default function ForecastItem({ day }) {
   const icon = day.weather[0].icon;
   const desc = day.weather[0].description;
 
+  return (
+    <div className="forecast-card">
+      <div className="forecast-day">{dayName}</div>
+      <div className="forecast-icon">
+        <img
+          src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt={desc}
+        />
+      </div>
+      <div className="forecast-temp">
+        {Math.round(day.main.temp_max)}° / {Math.round(day.main.temp_min)}°
+      </div>
+      <div className="forecast-desc">{desc}</div>
+    </div>
+  );
 }
